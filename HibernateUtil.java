845f7ecec3eac65c05f14c7e4eaf908064849b2a
Алексей <alex.yas001@gmai.com>
2021-03-13 12:16:47 +0300

    Add classes

---
+++ HibernateUtil.java
@@ -0,0 +1,23 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+class HybernateUtil {
+    private static SessionFactory sessionFactory;
+
+    private static SessionFactory buildSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().configure();
+        sessionFactory = configuration.buildSessionFactory(builder.build());
+        return sessionFactory;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null)
+            sessionFactory = buildSessionFactory();
+        return sessionFactory;
+    }
+}
